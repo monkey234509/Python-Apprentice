7a6918125bb37ad5bedb2b289b16c22452717a01
monkey234509 <yonatangrmay@stritassd.org>
2025-01-15 21:40:41 +0000

    I change the code and I made a traiger

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -18,6 +18,11 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # Make each side of the triangle a different color with 
 # tina.pencolor()
 
-... # Your code here
+# Your code here
+tina.forward(100)
+tina.left(120)
+tina.forward(100)
+tina.left(120)
+tina.forward(100)
 
 turtle.exitonclick()                    # Close the window when we click on it
